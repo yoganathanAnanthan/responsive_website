8dcb6856597cd08363b3d2cbb73839f2846092a7
yoganathanAnanthan <suthan.personal01@gmail.com>
2023-04-21 17:25:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = ()=>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = ()=>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = ()=>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+let navBar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = ()=>{
+    navBar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll=()=>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+var swiper = new Swiper(".product-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+// var swiper = new Swiper(".product-slider", {
+//     loop:true,
+//     spaceBetween: 20,
+//     autoplay:{
+//         delay:7500,
+//         disableOnInteraction: false,
+//     },
+//     centeredSlides:ture,
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//       },
+//       1020: {
+//         slidesPerView: 3,
+//       },
+//     },
+//   });
